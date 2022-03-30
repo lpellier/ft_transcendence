5ef6c75b6b90b6a77f975f8e316a9df283439947
ljurdant <ljurdant@student.42.com>
2022-03-30 15:09:12 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,35 +7,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-// Chat
-
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const PORT = 8080;
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:3000",
-    }
-});
-
-server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
-});
-
-
-
-io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
-
-    socket.emit('connection', null);
-    socket.on('disconnect', () => {
-    });
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg + ' user: ' + socket.username);
-        io.emit('chat message', msg, socket.username);
-    });
-    socket.on('set username', (username) => {
-        socket.username = username;
-        io.emit('new user', username);
-    })
-});
