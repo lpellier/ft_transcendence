fce56c8ae0154182ba4000deccfed2c70e8566dd
anBarot <barot.anne@gmail.com>
2022-04-01 23:37:42 +0200

    pretty though it doesn't work

---
+++ frontend/src/Components/Chat/Channels.tsx
@@ -0,0 +1,12 @@
+import React from'react';
+import '../../styles/Channels.css';
+
+function Channels() {
+	return (
+		<div className='channels'>
+			<h1 className='title-channels'>Channels</h1>
+		</div>
+	);
+}
+
+export default Channels;
