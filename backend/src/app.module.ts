318411c3b9d341ab4664668a376481e5cac3cdeb
Bruno Olmos Osorio <bolmos-o@student.42.fr>
2022-04-03 14:00:58 +0200

    removed unecessary cors-related lines and files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ChatGateway } from './chat/chat.gateway';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [AuthModule, UsersModule, ConfigModule.forRoot(), ChatModule],
-  providers: [ChatGateway],
 })
 export class AppModule {}
