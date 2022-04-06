fd6ef9453615f60b118bfc596a511991717edda2
Anne BAROT <abarot@e2r4p18.clusters.42paris.fr>
2022-04-06 20:14:15 +0200

    error page

--- frontend/src/styles/tsxStyles/AppBar/AuthStyle.tsx
+++ frontend/src/styles/tsxStyles/AppBar/AuthStyle.tsx
@@ -1,5 +1,16 @@
+import { styled } from '@mui/material/styles';
+
+const Title = {
+    fontSize: 'xx-large',
+    fontFamily: 'sans-serif',
+    color: 'rgb(150, 50, 200)',
+    fontWeight: '600',
+    textAlign: 'center',
+    filter: 'drop-shadow(2px 2px 1px black)',
+}
+
 const ButtonStackStyle = {
     textAlign: 'left',
 }
 
-export { ButtonStackStyle }
+export { ButtonStackStyle, Title }
---
+++ frontend/src/styles/tsxStyles/error.tsx
@@ -0,0 +1,10 @@
+const errorStyle =  {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%',
+}
+
+export { errorStyle }
