f293699747164b083fcbdb97b009e41409465294
Bruno OLMOS OSORIO <bolmos-o@e3r2p11.clusters.42paris.fr>
2022-05-16 19:56:25 +0200

    adding chat functions

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,39 +1,83 @@
 import { ConfigService } from "@nestjs/config";
 import { MessageBody, ConnectedSocket, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket } from "socket.io";
-import { User, Room} from '../interfaces'
+import { ChatService } from './chat.service';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
+
 @WebSocketGateway({
-	cors: {
+  cors: {
 	  origin: (new ConfigService).get("FRONT_URL"),
 	  credentials: true
-	},
-  })
+	}
+})
 export class ChatGateway {
-	@WebSocketServer()
+  constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer()
 	server: Socket;
+
+	@SubscribeMessage('create room') 
+	handleCreateRoom(@MessageBody()  createRoomDto: CreateRoomDto) {
+		// TODO add new room to database
+	}
+
+	// @SubscribeMessage('create correlation')
+	// handleCreateCorrelation(@MessageBody() data: any) {
+	// 	//TODO add new user/room correlation
+	// }
 
 	@SubscribeMessage('join room')
 	handleJoinRoom(@ConnectedSocket() client : Socket, @MessageBody() room_id: string ) {
-		
-		// if (room !== undefined)
-			// room_id = room.id.toString();
-		// console.log("room  = ",room_id)
-		// console.log("backend room = ", room);
 		client.join(room_id);
 	}
 
 	@SubscribeMessage('chat message')
 	handlemessage(@MessageBody() data: any) {
-		const message:string = data[0];
-		const user:User = data[1];
-		const room: Room = data[2];
-		// console.log("chat message:", message, ", room: ", room.id.toString());
-		this.server.to(room.id.toString()).emit('chat message', message, user, room)
-		// this.server.emit('chat message', message, user);
+		const content:string = data[0];
+		const user = data[1];
+		const room = data[2];
+		
+		// TODO add new message to database
+
+		this.server.to(room.id.toString()).emit('chat message', content, user, room)
 	}
 
-	// @SubscribeMessage('new username')
-	// handleSetUsername(@MessageBody() user: string) {
-	// 	this.server.emit('new user', user)
-	//   }
+	@SubscribeMessage('get rooms')
+	handleGetRooms(@MessageBody('id') id: number){
+		return this.chatService.getRoomsForUser(id);
+		// TODO return room list from user
+	}
+
+	@SubscribeMessage('get users')
+	handleGetUsers(@MessageBody('id') id: number) {
+		return this.chatService.getUsersInRoom(id);
+		// TODO return users list from user
+	}
+
+
+  // @SubscribeMessage('createChat')
+  // create(@MessageBody() createChatDto: CreateChatDto) {
+  //   return this.chatService.create(createChatDto);
+  // }
+
+  // @SubscribeMessage('findAllChat')
+  // findAll() {
+  //   return this.chatService.findAll();
+  // }
+
+  // @SubscribeMessage('findOneChat')
+  // findOne(@MessageBody() id: number) {
+  //   return this.chatService.findOne(id);
+  // }
+
+  // @SubscribeMessage('updateChat')
+  // update(@MessageBody() updateChatDto: UpdateChatDto) {
+  //   return this.chatService.update(updateChatDto.id, updateChatDto);
+  // }
+
+  // @SubscribeMessage('removeChat')
+  // remove(@MessageBody() id: number) {
+  //   return this.chatService.remove(id);
+  // }
 }
---
+++ backend/src/chat/chat.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+// import { CreateChatDto } from './dto/create-chat.dto';
+// import { UpdateChatDto } from './dto/update-chat.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class ChatService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async getRoomsForUser(id: number) {
+    let user = await this.prisma.user.findUnique({
+      where: {
+        id: id
+      },
+      include: {
+        rooms: true
+      }
+    });
+    return user.rooms
+  }
+  async getUsersInRoom(id: number) {
+
+  }
+  // create(createChatDto: CreateChatDto) {
+  //   return 'This action adds a new chat';
+  // }
+
+  // findAll() {
+  //   return `This action returns all chat`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} chat`;
+  // }
+
+  // update(id: number, updateChatDto: UpdateChatDto) {
+  //   return `This action updates a #${id} chat`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} chat`;
+  // }
+}
