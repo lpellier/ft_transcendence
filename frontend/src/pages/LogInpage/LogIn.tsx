8ed83e071bc11b8ca6d21a1c7ae6dfad6002ae14
Anne BAROT <abarot@e2r11p22.clusters.42paris.fr>
2022-06-07 21:16:30 +0200

    adding conditionnal login

--- frontend/src/pages/LogInpage/LogIn.tsx
+++ frontend/src/pages/LogInpage/LogIn.tsx
@@ -1,44 +1,49 @@
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import LoginIcon from '@mui/icons-material/Login';
 import { ThemeProvider } from '@mui/styles';
 import WebhookIcon from '@mui/icons-material/Webhook';
+import NewWindow from 'react-new-window'
 
+import {Navigate} from 'react-router-dom'
+import {token} from 'index'
 import {orangeTheme} from 'components/Themes'
 import {Title, ButtonStyle, LinkStyle, IconStyle} from "../../styles/tsxStyles/LogIn"
+import '../../styles/Other/LoginWindow.css'
 
 import axios from "axios";
 
 const AuthAPI = "http://127.0.0.1:3001/auth"
 
-function LogInButton(props: {login: any})
- {
+function LogInButton(props: {login: any}) {
+
 	return (
 		<ThemeProvider theme={orangeTheme}>
-                <Button 
-					onClick={props.login}
-					sx={ButtonStyle}
-                    variant="contained"
-                    startIcon={<LoginIcon />}
-                    size="large"
-                    color="primary"
-					>
-                  Log in
-                </Button>
-        </ThemeProvider>
-      );
+			<Button 
+				onClick={props.login}
+				sx={ButtonStyle}
+				variant="contained"
+				startIcon={<LoginIcon />}
+				size="large"
+				color="primary"
+				>
+			  Log in
+			</Button>
+		</ThemeProvider>
+	)
 }
 
-export default function LogIn() {
+function LoginWindow() {
 	
-	 function getRequest() {
-		const newWin = window.open(AuthAPI, 'newwindow', 'width=400, height=400');
+	function getRequest() {
+
 		axios.get('http://127.0.0.1:3001',
 		{'headers': {
 			// 'Origin':'http://127.0.0.1:3000',
 			'Access-Control-Allow-Origin': 'http://127.0.0.1:3000',
 		}
-	})
+		})
 		.then(res => {
 			console.log("Get request success")
 			const test_data = res.data;
@@ -49,6 +54,51 @@ export default function LogIn() {
 		});
 	}
 
+	if (!{token})
+	{
+		return (
+			<NewWindow
+				title="Login page" >
+				<nav>
+					<a href={AuthAPI} style={LinkStyle}>
+					  	<LogInButton login={getRequest}/>
+					</a>
+				</nav>
+			</NewWindow>
+		);
+	}
+	return (
+		<Navigate to={"/home"} />
+	)
+}
+
+function LogInWindowButton()
+ {
+	const [isOpen, setOpen] = useState(false);
+
+	const openWindow = () => {
+		setOpen(!isOpen);
+	};
+
+	return (
+		<ThemeProvider theme={orangeTheme}>
+                <Button 
+					onClick={openWindow}
+					sx={ButtonStyle}
+                    variant="contained"
+                    startIcon={<LoginIcon />}
+                    size="large"
+                    color="primary"
+					>
+                  Log in
+                </Button>
+				{isOpen && <LoginWindow />}
+        </ThemeProvider>
+      );
+}
+
+export default function LogIn() {
+
     return (
         	<Stack spacing={10} sx={Title}>
 				<div>
@@ -56,11 +106,7 @@ export default function LogIn() {
 					<WebhookIcon sx={IconStyle}/>
 					Pong
 				</div>
-				<nav>
-					{/* <a href={AuthAPI} style={LinkStyle}> */}
-					  	<LogInButton login={getRequest}/>
-					{/* </a> */}
-				</nav>
+				<LogInWindowButton />
         	</Stack>
         );
 }
