29f4fc4407239241220f8574312123b3d29ac464
NULL <bruno.osorio@me.com>
2022-06-27 18:18:48 +0200

    added redirection and get method

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -9,40 +9,51 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 @Controller('auth')
 export class AuthController {
 	constructor(
-		private authService: AuthService) {}
+		private authService: AuthService) { }
 
 	@UseGuards(OAuth2AuthGuard)
 	@Get()
-	@Redirect('http://127.0.0.1:3000/profile')
-	async login(@Req() req, @Res({passthrough: true}) res: Response) {
+	@Redirect()
+	async login(@Req() req, @Res({ passthrough: true }) res: Response) {
 		const isAuthenticated = !req.user.tfa;
 		const authentication = await this.authService.login(req.user.id, isAuthenticated);
-		res.cookie(authentication.type, authentication.token, { sameSite: 'strict' , httpOnly: true });
+		res.cookie(authentication.type, authentication.token, { sameSite: 'strict', httpOnly: true });
+		if (isAuthenticated === true) {
+			return ({ url: 'http://127.0.0.1:3000/profile' })
+		} else {
+			return ({ url: 'http://127.0.0.1:3000/tfauth' })
+		}
 	}
 
 	@Get('mock')
 	@Redirect('http://127.0.0.1:3000/profile')
-	async mockLogin(@Req() req, @Res({passthrough: true}) res: Response) {
+	async mockLogin(@Req() req, @Res({ passthrough: true }) res: Response) {
 		const user = await this.authService.getMock();
 		const isAuthenticated = !user.tfa;
 		const authentication = await this.authService.login(user.id, isAuthenticated);
-		res.cookie(authentication.type, authentication.token, { sameSite: 'strict' , httpOnly: true });
+		res.cookie(authentication.type, authentication.token, { sameSite: 'strict', httpOnly: true });
 	}
 
 
 	@UseGuards(JwtOtpAuthGuard)
+	@Get('google-authenticator')
+	async googleAuthenticatorLoginCheck() {
+		return true;
+	}
+
+	@UseGuards(JwtOtpAuthGuard)
 	@Post('google-authenticator')
-	async google_authenticator_login(@Req() req, @Res({passthrough: true}) res: Response, @Body() otp: ValidateOtpDto) {
+	async googleAuthenticatorLogin(@Req() req, @Res({ passthrough: true }) res: Response, @Body() otp: ValidateOtpDto) {
 		const validated = await this.authService.validateGoogleAuthenticatorToken(req.user.id, otp)
 		if (validated === true) {
 			const authentication = await this.authService.login(req.user.id, true);
-			res.cookie(authentication.type, authentication.token, { sameSite: 'strict' , httpOnly: true });
+			res.cookie(authentication.type, authentication.token, { sameSite: 'strict', httpOnly: true });
 		}
 	}
 
 	@UseGuards(JwtAuthGuard)
 	@Get('logout')
-	async logout(@Res({passthrough: true}) res: Response) {
-		res.cookie('jwt', "", { expires: new Date(), sameSite: 'strict' , httpOnly: true });
+	async logout(@Res({ passthrough: true }) res: Response) {
+		res.cookie('jwt', "", { expires: new Date(), sameSite: 'strict', httpOnly: true });
 	}
 }
