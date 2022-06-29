3d00450a5a7ff41eb41481b647bae5c893cb9f9a
Louis JURDANT <ljurdant@studebt.42.fr>
2022-06-29 20:18:30 +0200

    merge

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,8 @@ import { UsersModule } from './users/users.module';
 import { StatsModule } from './stats/stats.module';
 import { ChatModule } from './chat/chat.module';
 import { GameModule } from './game/game.module';
-import { FriendsModule } from './friends/friends.module';
-import { BlockedModule } from './blocked/blocked.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, StatsModule, ConfigModule.forRoot({isGlobal: true}), ChatModule, GameModule, FriendsModule, BlockedModule],
+  imports: [AuthModule, UsersModule, StatsModule, ConfigModule.forRoot({isGlobal: true}), ChatModule, GameModule],
 })
 export class AppModule {}
