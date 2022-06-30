7a516dbc30d255e8aa5cb96eace3758911fcb901
lpellier <lpellier@student.42.fr>
2022-06-30 13:18:28 +0200

    final fix on player name animation

--- frontend/src/components/Game/classes/MovingText.ts
+++ frontend/src/components/Game/classes/MovingText.ts
@@ -9,6 +9,8 @@ class MovingText {
     text : string;
     initial_speed : number;
     slow_speed : number;
+
+    transition_speed : number;
     
     constructor(x : number, y : number, left : boolean, text : string) {
         this.initial_speed = left ? consts.DIAGONAL * 0.030 : -consts.DIAGONAL * 0.030;
@@ -21,22 +23,24 @@ class MovingText {
         
         this.text = text;
         this.moving = false;
+
+        this.transition_speed = consts.DIAGONAL / 400;
     }
 
     calculatePos() {
         if (this.left) {
             if (this.pos.x > consts.WIDTH * 0.2 && this.pos.x < consts.WIDTH * 0.6)
-                this.vel.lerp(new Vector([this.slow_speed, 0]), 1.25);
+                this.vel.lerp(new Vector([this.slow_speed, 0]), this.transition_speed);
             else if (this.pos.x  > consts.WIDTH * 0.6)
-                this.vel.lerp(new Vector([this.initial_speed * 4, 0]), 2);
+                this.vel.lerp(new Vector([this.initial_speed * 4, 0]), this.transition_speed * 2);
             if (this.pos.x > consts.WIDTH + this.text.length * consts.std_font_size)
                 this.moving = false;
         }
         else {
             if (this.pos.x > consts.WIDTH * 0.4 && this.pos.x < consts.WIDTH * 0.8)
-                this.vel.lerp(new Vector([this.slow_speed, 0]), 1.25);
+                this.vel.lerp(new Vector([this.slow_speed, 0]), this.transition_speed);
             else if (this.pos.x  < consts.WIDTH * 0.4)
-                this.vel.lerp(new Vector([this.initial_speed * 4, 0]), 2);
+                this.vel.lerp(new Vector([this.initial_speed * 4, 0]), this.transition_speed * 2);
             if (this.pos.x + this.text.length * consts.std_font_size < 0)
                 this.moving = false;
         }
