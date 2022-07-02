d3dce96d9ee3f7e81ea0dfb055575be4b54e7a02
anBarot <barot.anne@gmail.com>
2022-07-02 16:25:03 +0200

    adding skillbar

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,10 +1,10 @@
+import {useEffect, useState, useRef} from 'react'
 import Stack from '@mui/material/Stack'
-import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import StatsBoards from './StatsBoards'
 import Typography from '@mui/material/Typography'
 import {User} from 'interfaces'
-import {PlayerBarStyle} from "../../styles/tsxStyles/Home";
+import {PlayerBarStyle, SkillBarContourStyle, TitleStyle} from "../../styles/tsxStyles/Home";
 import './../../styles/Other/SkillBar.css'
 
 const OverallBoxStyle = {
@@ -13,14 +13,35 @@ const OverallBoxStyle = {
 	justifyContent: 'center',
 }
 
+function SkillBar(props: {progression: number}) {
+	const [length, setLength] = useState(0);
+	const totwidth = length + "px";
+	const SkillBox = useRef();
 
-function PlayerInfoBand(props: {level: number}) {
+	useEffect(() => {
+		//@ts-ignore: next-line
+		setLength((props.progression / 100) * SkillBox.current.offsetWidth);
+	}, [props.progression]);
+
+	return (
+		<Box ref={SkillBox} sx={SkillBarContourStyle} >
+			<div style={{width: totwidth}} className="SkillBar">
+					<Typography variant="subtitle2" sx={TitleStyle}>
+						{props.progression}%
+					</Typography>
+			</div>
+		</Box>
+	);
+}
+
+function PlayerInfoBand(props: {level: number, xp: number}) {
 	return (
 			<Box sx={PlayerBarStyle}>
 				<Stack spacing={2}>
 					<Typography variant="h6">
 						Level {props.level}
 					</Typography>
+					<SkillBar progression={props.xp}/>
 				</Stack>
 			</Box>
 	);
@@ -32,7 +53,7 @@ export default function Profile(props: {user: User | undefined, users: User[]}) 
 		<Box sx={OverallBoxStyle}>
 			{props.user?
 				<Stack spacing={1}>
-					<PlayerInfoBand level={props.user.level}/>
+					<PlayerInfoBand level={props.user.level} xp={props.user.level}/>
 					<StatsBoards user={props.user} users={props.users}/>
 				</Stack>
 					:
