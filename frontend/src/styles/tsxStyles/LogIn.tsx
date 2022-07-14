ff687fce6108c52b3aa0474e76c228ecf1b8c737
Bruno Olmos Osorio <bruno.osorio@outlook.com>
2022-07-14 20:18:14 +0200

    added some of the styling changes

--- frontend/src/styles/tsxStyles/LogIn.tsx
+++ frontend/src/styles/tsxStyles/LogIn.tsx
@@ -1,28 +1,28 @@
 const Title = {
-    fontWeight: '400',
-    fontSize: '10vw',
-    
-    color: 'rgb(255, 69, 0)',
-    filter: 'drop-shadow(20px 15px 4px black)',
-    
-    alignItems: 'center',
-	display: 'flex',
-	paddingTop: '40vh',
-}
+  fontWeight: "400",
+  fontSize: "10vw",
+
+  color: "rgb(255, 69, 0)",
+  filter: "drop-shadow(20px 15px 4px black)",
+
+  textAlign: "center",
+  display: "flex",
+  paddingTop: "36vh",
+};
 
 const IconStyle = {
-    fontSize: '5vw',
-}
+  fontSize: "5vw",
+};
 
 const ButtonStyle = {
-	backgroundColor: 'rgb(140, 50, 120)',
-    textShadow: '1px 1px 2px black',
-}
+  backgroundColor: "rgb(140, 50, 120)",
+  textShadow: "1px 1px 2px black",
+};
 
-const LinkStyle = { 
-    textDecoration: 'none',
-	display: 'flex',
-	justifyContent: 'center',
-}
+const LinkStyle = {
+  textDecoration: "none",
+  display: "flex",
+  justifyContent: "center",
+};
 
-export { Title, ButtonStyle, LinkStyle, IconStyle }
+export { Title, ButtonStyle, LinkStyle, IconStyle };
