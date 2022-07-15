63a0f834e6cc93e409c6f63fa4170a6e5cea4797
Bruno Olmos Osorio <bruno.osorio@outlook.com>
2022-07-15 16:23:56 +0200

    slightly better readability

--- frontend/src/routes/NotFound.tsx
+++ frontend/src/routes/NotFound.tsx
@@ -2,9 +2,9 @@ import { Box, Typography } from "@mui/material";
 
 export default function NotFound() {
   return (
-    <Box sx={{paddingTop:"20vh"}}>
-      <Typography variant="h2" color="white" textAlign={"center"}>404</Typography>
-      <Typography variant="body1" color="white" textAlign={"center"}>Page not found</Typography>
+    <Box sx={{paddingTop:"20vh", color: "white"}}>
+      <Typography variant="h2"  textAlign={"center"} sx={{fontWeight: "bold"}}>404</Typography>
+      <Typography variant="body1" textAlign={"center"} sx={{fontWeight: "bold"}}>Page not found</Typography>
     </Box>
   );
 }
