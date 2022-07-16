61f3cdb346dffa7a4d63a506b57d750bf5d89f0c
Bruno Olmos Osorio <bruno.osorio@outlook.com>
2022-07-16 17:51:48 +0200

    modifications on table formatting

--- frontend/src/components/Profile/Trophies.tsx
+++ frontend/src/components/Profile/Trophies.tsx
@@ -1,73 +1,84 @@
-import {	Stack,
-			Typography,
-			Grid
-} from "@mui/material"
-import Star from "../../images/Trophies/star.png"
-import ThreeStars from "../../images/Trophies/threestars.png"
-import TheQuitter from "../../images/Trophies/quitter.png"
-import Frankencat from "../../images/Trophies/frankencat.png"
+import { Stack, Typography } from "@mui/material";
+import Star from "../../images/Trophies/star.png";
+import ThreeStars from "../../images/Trophies/threestars.png";
+import TheQuitter from "../../images/Trophies/quitter.png";
+import Frankencat from "../../images/Trophies/frankencat.png";
 
 enum achievements {
-	ONESTAR,
-	THREESTARS,
-	QUIT,
-	CHANGEAVATAR,
+  ONESTAR,
+  THREESTARS,
+  QUIT,
+  CHANGEAVATAR,
 }
 
 const ImgStyle = {
-	width: "4vw",
-	minHeight: "4vw",
+  width: "4vw",
+  minHeight: "4vw",
 
-	padding: 1,
-}
+  padding: 1,
+};
 
 const ATrophyStyle = {
-	border: "2px solid black",
-	backgroundColor: "rgb(100, 100, 200, 0.2)",
-	alignItems: 'center',
-}
+  border: "2px solid black",
+  backgroundColor: "rgb(100, 100, 200, 0.2)",
+  alignItems: "center",
+};
 
-function ATrophie(props: {image: string, title: string, label: string}) {
+function ATrophie(props: { image: string; title: string; label: string }) {
   return (
-		<Grid container rowSpacing={1} sx={ATrophyStyle}>
-			<Grid xs={2}>
-				<img src={props.image} alt="Trophy" style={ImgStyle} />
-			</Grid>
-			<Stack>
-				<Typography variant="subtitle2">
-					{props.title}
-				</Typography>
-				<Typography variant="subtitle1" color="black">
-					{props.label}
-				</Typography>
-			</Stack>
-		</Grid>
+    <Stack direction="row" spacing={2} sx={ATrophyStyle}>
+      <img src={props.image} alt="Trophy" style={ImgStyle} />
+      <Stack>
+        <Typography variant="subtitle2">{props.title}</Typography>
+        <Typography variant="subtitle1" color="black">
+          {props.label}
+        </Typography>
+      </Stack>
+    </Stack>
   );
 }
 
 function OneWinTrophy() {
-	return (
-		<ATrophie image={Star} label="You're the star, man!" title="First win" />
-  	);
+  return (
+    <ATrophie image={Star} label="You're the star, man!" title="First win" />
+  );
 }
 
 function ThreeWinsTrophy() {
-	return (
-		<ATrophie image={ThreeStars} label="Like taking candy to a baby!" title="Three wins" />
-  	);
+  return (
+    <ATrophie
+      image={ThreeStars}
+      label="Like taking candy to a baby!"
+      title="Three wins"
+    />
+  );
 }
 
 function QuitTrophy() {
-	return (
-		<ATrophie image={TheQuitter} label="Baby please don't go" title="Quit the game"/>
-  	);
+  return (
+    <ATrophie
+      image={TheQuitter}
+      label="Baby please don't go"
+      title="Quit the game"
+    />
+  );
 }
 
 function ChangeAvatarTrophy() {
-	return (
-		<ATrophie image={Frankencat} label="He has changed, for sure" title="Changed avatar"/>
-  	);
+  return (
+    <ATrophie
+      image={Frankencat}
+      label="He has changed, for sure"
+      title="Changed avatar"
+    />
+  );
 }
 
-export {ChangeAvatarTrophy, QuitTrophy, OneWinTrophy, 
-		ThreeWinsTrophy, achievements}
+export {
+  ChangeAvatarTrophy,
+  QuitTrophy,
+  OneWinTrophy,
+  ThreeWinsTrophy,
+  achievements,
+};
+
